3d114612d727df805be5a7ae2e4e2e470977d0ce
Alex-sqrt <75654963+Alex-sqrt@users.noreply.github.com>
2020-12-19 04:21:39 +0300

    Layout
    Файл с командами для Encounter

--- Sqrt2_PaR.tcl
+++ Sqrt2_PaR.tcl
@@ -1,7 +1,7 @@
 #######################################################
 #                                                     
 #  Encounter Command Logging File                     
-#  Created on Wed Dec  9 18:43:59 2020                
+#  Created on Fri Dec 18 03:47:06 2020                
 #                                                     
 #######################################################
 
@@ -28,9 +28,9 @@ set init_design_settop 0
 set init_gnd_net VSS
 set init_io_file ../Source/Module_pins
 set init_lef_file {/Cadence/Libs/X_FAB/XKIT/xt018/cadence/v5_0/techLEF/v5_0_2/xt018_xx43_MET4_METMID_METTHK.lef /Cadence/Libs/X_FAB/XKIT/xt018/diglibs/D_CELLS_HD/v4_0/LEF/v4_0_0/xt018_D_CELLS_HD.lef}
-set init_mmmc_file ../Source/MMMC.tcl
+set init_mmmc_file ../Scripts/MMMC.tcl
 set init_pwr_net VDD
-set init_verilog ../Source/Synthesis/Sqrt2_synth.v
+set init_verilog ../Outputs/Sqrt2_synth.v
 set lsgOCPGainMult 1.000000
 set pegDefaultResScaleFactor 1.000000
 set pegDetailResScaleFactor 1.000000
@@ -38,29 +38,10 @@ set timing_library_float_precision_tol 0.000010
 set timing_library_load_pin_cap_indices {}
 set tso_post_client_restore_command {update_timing ; write_eco_opt_db ;}
 init_design
-pan -0.194 -0.427
-pan -2.886 -3.515
-pan -34.237 -18.139
-pan -2.425 -64.758
-pan -162.056 -23.151
-getIoFlowFlag
-setIoFlowFlag 0
 ############################# Floorplan ##########################
-floorPlan -site core_hd -r 0.976232353262 0.699998 10 10 10 10
+floorPlan -site core_hd -r 1 0.7 10 10 10 10
 uiSetTool select
-getIoFlowFlag
-fit
-setIoFlowFlag 0
-floorPlan -site core_hd -r 0.976232353262 0.699998 10.08 10.37 10.08 10.37
-uiSetTool select
-getIoFlowFlag
-fit
-clearGlobalNets
 ############################ Пины питания #######################################
-globalNetConnect VDD -type pgpin -pin vdd -inst * -module {}
-globalNetConnect VDD -type tiehi -inst * -module {}
-globalNetConnect VSS -type pgpin -pin gnd -inst * -module {}
-globalNetConnect VSS -type tielo -inst * -module {}
 clearGlobalNets
 globalNetConnect VDD -type pgpin -pin vdd -inst * -module {}
 globalNetConnect VDD -type tiehi -inst * -module {}
@@ -81,12 +62,12 @@ set sprCreateIeStripeLayers {}
 set sprCreateIeStripeWidth 10.0
 set sprCreateIeStripeSpacing 2.0
 set sprCreateIeStripeThreshold 1.0
-addStripe -skip_via_on_wire_shape Noshape -block_ring_top_layer_limit MET3 -max_same_layer_jog_length 6 -padcore_ring_bottom_layer_limit MET1 -set_to_set_distance 100 -skip_via_on_pin Standardcell -stacked_via_top_layer METTPL -padcore_ring_top_layer_limit MET3 -spacing 0.28 -merge_stripes_value 3.15 -layer MET2 -block_ring_bottom_layer_limit MET1 -width 3 -nets {VSS VDD} -stacked_via_bottom_layer MET1
+addStripe -skip_via_on_wire_shape Noshape -block_ring_top_layer_limit MET3 -max_same_layer_jog_length 6 -padcore_ring_bottom_layer_limit MET1 -set_to_set_distance 100 -skip_via_on_pin Standardcell -stacked_via_top_layer METTPL -padcore_ring_top_layer_limit MET3 -spacing 0.28 -merge_stripes_value 3.15 -layer MET2 -block_ring_bottom_layer_limit MET1 -width 3 -nets {VDD VSS} -stacked_via_bottom_layer MET1
 #################### Horizontal stripes ################################
 sroute -connect { blockPin padPin padRing corePin floatingStripe } -layerChangeRange { MET1 METTPL } -blockPinTarget { nearestTarget } -padPinPortConnect { allPort oneGeom } -padPinTarget { nearestTarget } -corePinTarget { firstAfterRowEnd } -floatingStripeTarget { blockring padring ring stripe ringpin blockpin followpin } -allowJogging 1 -crossoverViaLayerRange { MET1 METTPL } -nets { VDD VSS } -allowLayerChange 1 -blockPin useLef -targetViaLayerRange { MET1 METTPL }
 redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
 ###################### Pre_Place отчет ##############################
-timeDesign -prePlace -idealClock -pathReports -drvReports -slackReports -numPaths 50 -prefix Sqrt2_prePlace -outDir timingReports_F20_U6_new
+timeDesign -prePlace -idealClock -pathReports -drvReports -slackReports -numPaths 50 -prefix Sqrt2_prePlace -outDir timingReports_F21_U5
 ###################### Place ####################################
 setMultiCpuUsage -localCpu 8 -cpuPerRemoteHost 1 -remoteHost 0 -keepLicense true
 setDistributeHost -local
@@ -94,28 +75,25 @@ setPlaceMode -fp false
 placeDesign -inPlaceOpt
 redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
 ########################### Pre-CTS отчет ##################################
-timeDesign -preCTS -idealClock -pathReports -drvReports -slackReports -numPaths 50 -prefix Sqrt2_preCTS -outDir timingReports_F20_U6_new
+timeDesign -preCTS -idealClock -pathReports -drvReports -slackReports -numPaths 50 -prefix Sqrt2_preCTS -outDir timingReports_F21_U5
 redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
-timeDesign -preCTS -hold -idealClock -pathReports -slackReports -numPaths 50 -prefix Sqrt2_preCTS -outDir timingReports_F20_U6_new
+timeDesign -preCTS -hold -idealClock -pathReports -slackReports -numPaths 50 -prefix Sqrt2_preCTS -outDir timingReports_F21_U5
 ############################ Pre-CTS оптимизация ###########################
 setOptMode -fixCap true -fixTran true -fixFanoutLoad true
 optDesign -preCTS
-############################ Создание Buffer tree ##########################
-createClockTreeSpec -bufferList {BUHDX0 BUHDX1 BUHDX12 BUHDX2 BUHDX3 BUHDX4 BUHDX6 BUHDX8} -file Clock_F20_U6_new.ctstch
+############################ Создание Clock tree ##########################
+createClockTreeSpec -bufferList {BUHDX0 BUHDX1 BUHDX12 BUHDX2 BUHDX3 BUHDX4 BUHDX6 BUHDX8} -file Clock_F21_U5_new.ctstch
 setCTSMode -engine ck
-clockDesign -specFile Clock_F20_U6_new.ctstch -outDir clock_report_F20_U6_new -fixedInstBeforeCTS
+clockDesign -specFile Clock_F21_U5_new.ctstch -outDir clock_report_F21_U5 -fixedInstBeforeCTS
 redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
 ########################### Post-CTS отчеты ##########################
-timeDesign -postCTS -pathReports -drvReports -slackReports -numPaths 50 -prefix Sqrt2_postCTS -outDir timingReports_F20_U6_new
+timeDesign -postCTS -pathReports -drvReports -slackReports -numPaths 50 -prefix Sqrt2_postCTS -outDir timingReports_F21_U5
 redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
-timeDesign -postCTS -hold -pathReports -slackReports -numPaths 50 -prefix Sqrt2_postCTS -outDir timingReports_F20_U6_new
+timeDesign -postCTS -hold -pathReports -slackReports -numPaths 50 -prefix Sqrt2_postCTS -outDir timingReports_F21_U5
 ########################### Post-CTS оптимизация #####################
 setOptMode -fixCap true -fixTran true -fixFanoutLoad true
 optDesign -postCTS
 optDesign -postCTS -hold
-setOptMode -fixCap false -fixTran false -fixFanoutLoad false
-optDesign -postCTS -incr
-optDesign -postCTS -hold -incr
 ########################### Routing #####################################
 setNanoRouteMode -quiet -timingEngine {}
 setNanoRouteMode -quiet -routeWithSiPostRouteFix 0
@@ -125,38 +103,34 @@ setNanoRouteMode -quiet -drouteEndIteration default
 setNanoRouteMode -quiet -routeWithTimingDriven false
 setNanoRouteMode -quiet -routeWithSiDriven false
 routeDesign -globalDetail
-pan -26.207 36.098 
 ############################# Post-Route оптимизация и отчеты ###########################
-setAnalysisMode -analysisType onChipVariation -skew true -clockPropagation
 setAnalysisMode -analysisType onChipVariation -skew true -clockPropagation sdcControl
 redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
-timeDesign -postRoute -pathReports -drvReports -slackReports -numPaths 50 -prefix Sqrt2_postRoute -outDir timingReports_F20_U6_new
+timeDesign -postRoute -pathReports -drvReports -slackReports -numPaths 50 -prefix Sqrt2_postRoute -outDir timingReports_F21_U5
 redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
-timeDesign -postRoute -hold -pathReports -slackReports -numPaths 50 -prefix Sqrt2_postRoute -outDir timingReports_F20_U6_new
+timeDesign -postRoute -hold -pathReports -slackReports -numPaths 50 -prefix Sqrt2_postRoute -outDir timingReports_F21_U5
 setOptMode -fixCap true -fixTran true -fixFanoutLoad true
 optDesign -postRoute
 optDesign -postRoute -hold
-####################### Добавление филлеров #####################################
 getFillerMode -quiet
+####################### Добавление филлеров #####################################
 addFiller -cell FEED7HD FEED5HD FEED3HD FEED2HD FEED25HD FEED1HD FEED15HD FEED10HD DECAP10HD DECAP15HD DECAP25HD DECAP3HD DECAP5HD DECAP7HD -prefix FILLER
-setVerifyGeometryMode -area { 0 0 0 0 } -minWidth true -minSpacing true -minArea true -sameNet true -short true -overlap true -offRGrid false -offMGrid true -mergedMGridCheck true -minHole true -implantCheck true -minimumCut true -minStep true -viaEnclosure true -antenna true -insuffMetalOverlap true -pinInBlkg false -diffCellViol true -sameCellViol false -padFillerCellsOverlap true -routingBlkgPinOverlap true -routingCellBlkgOverlap true -regRoutingOnly false -stackedViasOnRegNet false -wireExt true -useNonDefaultSpacing false -maxWidth true -maxNonPrefLength -1 -error 1000
-verifyGeometry
-setVerifyGeometryMode -area { 0 0 0 0 }
-######################### Sign-off отчеты ###########################
+######################### Экстракция и Sign-off отчеты ###########################
 setExtractRCMode -engine postRoute -effortLevel signoff
 extractRC
 redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
-timeDesign -signoff -pathReports -drvReports -slackReports -numPaths 50 -prefix Sqrt2_signOff -outDir timingReports_F20_U6_new
+timeDesign -signoff -pathReports -drvReports -slackReports -numPaths 50 -prefix Sqrt2_signOff -outDir timingReports_F21_U5
 redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
-timeDesign -signoff -hold -pathReports -slackReports -numPaths 50 -prefix Sqrt2_signOff -outDir timingReports_F20_U6_new
+timeDesign -signoff -hold -pathReports -slackReports -numPaths 50 -prefix Sqrt2_signOff -outDir timingReports_F21_U5
 all_hold_analysis_views 
-all_setup_analysis_views 
-########################## Экспорт выходных файлов ###############################
-write_sdf ../Outputs/Sqrt2.sdf
-saveNetlist ../Outputs/Sqrt2_netlist.v
-saveNetlist ../Outputs/Sqrt2_phys.v -includePhysicalCell {FEED7HD FEED5HD FEED3HD FEED2HD FEED25HD FEED1HD FEED15HD FEED10HD DECAP7HD DECAP5HD DECAP3HD DECAP25HD DECAP15HD DECAP10HD}
+all_setup_analysis_views
+########################## Экспорт выходных файлов ############################### 
+write_sdf ../Outputs_F21_U5/Sqrt2.sdf
+saveNetlist ../Outputs_F21_U5/Sqrt2_netlist.v
+saveNetlist ../Outputs_F21_U5/Sqrt2_phys.v -includePhysicalCell {FEED7HD FEED5HD FEED3HD FEED2HD FEED25HD FEED1HD FEED15HD FEED10HD DECAP7HD DECAP5HD DECAP3HD DECAP25HD DECAP15HD DECAP10HD}
 global dbgLefDefOutVersion
 set dbgLefDefOutVersion 5.8
-defOut -floorplan -netlist -routing ../Outputs/Sqrt2.def
+defOut -floorplan -netlist -routing ../Outputs_F21_U5/Sqrt2.def
 set dbgLefDefOutVersion 5.8
+saveDesign Sqrt2_F21_U5.enc
 
